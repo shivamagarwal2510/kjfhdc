4b0cf122c9140233eeba89e75250aae9585adbe9
Shivam Agarwal <99248722+shivamagarwal2510@users.noreply.github.com>
2025-05-26 17:56:35 +0530

    Initial commit with project files

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+import avatarImage from "./assets/test.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="avatar-container">
+        <img
+          src={avatarImage}
+          alt="Anime style avatar with glasses"
+          className="avatar-image"
+        />
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
